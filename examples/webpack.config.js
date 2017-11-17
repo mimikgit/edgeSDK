94339650d6333296f92226633da5ca8f8e182b3d
https://github.com/mimikgit/edgeSDK <mbarut@gmail.com>
2017-11-17 14:53:06 -0800

    Add existing file

---
+++ examples/webpack.config.js
@@ -0,0 +1,55 @@
+
+var webpack = require('webpack');
+var path = require('path');
+var Promise = require("bluebird");
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: SRC_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'index.js'
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            screw_ie8: true,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            sequences: true,
+            dead_code: true,
+            evaluate: true,
+            join_vars: true,
+            if_return: true
+        },
+        output: {
+            comments: false
+        }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+            presets: ['es2015', 'stage-0']
+        },
+        loader: "babel-loader",
+      },
+    ]
+  }
+};
+
+module.exports = config;
