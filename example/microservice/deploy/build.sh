50fee38a9e867dfdfc213d1e9aebc284a46f671a
Zhi <zhixianye@hotmail.com>
2019-05-06 11:48:01 -0700

    Update build.sh

--- example/microservice/deploy/build.sh
+++ example/microservice/deploy/build.sh
@@ -1,5 +1,25 @@
-cp ../build/index.js ./
-sudo docker build -t example-v1 .
-sudo docker save -o example-v1.tar example-v1
-sudo chmod 666 example-v1.tar
-sudo docker rmi example-v1
+#!/bin/bash
+
+unameOut="$(uname -s)"
+
+commands=(
+	"cp ../build/index.js ./"
+	"docker build -t example-v1 ."
+	"docker save -o example-v1.tar example-v1"
+	"chmod 666 example-v1.tar"
+	"docker rmi example-v1")
+
+case "${unameOut}" in
+	Linux*)     machine=Linux;;
+	Darwin*)    machine=Mac;;
+	MINGW*)     machine=MinGW;;
+	*)          machine="UNKNOWN:${unameOut}"
+esac
+
+for i in "${commands[@]}"; do
+	if [ ${machine} == "Mac" ] || [ ${machine} == "Linux" ]; then
+		sudo sh -c "$i"
+	elif [ ${machine} == "MinGW" ]; then
+		sh -c "$i"
+	fi
+done
